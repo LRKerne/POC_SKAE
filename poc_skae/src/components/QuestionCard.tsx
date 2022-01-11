1794e2228953bcc8d6a3d63db37c13dc4a71aa0e
LRKerne <LRKerne@protonmail.com>
2022-01-11 01:20:46 -0300

    Finished Quiz, Created Login Component but need to make them sequence

---
+++ poc_skae/src/components/QuestionCard.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import CardHeader from "@material-ui/core/CardHeader";
+import { Button } from "@material-ui/core";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    info: {
+      textAlign: "center",
+      margin: `${theme.spacing(0)} auto`,
+      background: "#6b9798",
+    },
+    answerBtn: {
+      marginTop: theme.spacing(2),
+    },
+    header: {justify: "center",
+    alignitems: "center",
+
+      marginTop: theme.spacing(10),
+    },
+    answercont: {
+      justifyContent:"center",
+      flexGrow: 1,
+      background: "#6b9798",
+    },
+  })
+);
+
+//state type
+
+type Props ={
+  question:string;
+  answers: string[];
+  callback: any;
+  userAnswer: any;
+  questionNr: number;
+  totalQuestions: number;
+}
+
+const QuestionCard: React.FC<Props> =({
+  question, 
+  answers,
+  callback,
+  questionNr,
+  totalQuestions,
+  userAnswer
+}) =>{
+  const classes = useStyles();
+  
+  return(
+  <div>
+    <Card>
+    {/* <CardHeader className={classes.header} title={`Question ${questionNr} / ${totalQuestions}`} /> */}
+      <CardContent className={classes.info}>
+      Question: {questionNr} / {totalQuestions}
+      <p dangerouslySetInnerHTML={{__html: question }} />
+      </CardContent>
+    <CardActions className = {classes.answercont}>
+      {answers.map((answer) => (
+          <Button 
+          key={answer}
+          variant="contained"
+          size="small"
+          color="inherit"
+          disabled={userAnswer}
+          className={classes.answerBtn}
+          value= {answer} 
+          onClick={callback}>
+            <span dangerouslySetInnerHTML={{__html: answer}}></span>
+            </Button>
+      ))}
+      </ CardActions>
+      </Card>
+
+  </div>
+
+);
+
+};
+
+export default QuestionCard;
